b499102a159cf3f86af3c018063638eee2642841
Incognito Svetoslav <venom008@gmail.com>
2015-06-02 00:20:17 +0500

    Add tests project

---
+++ Hg.Net/Hg.Net.Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Hg.Net.Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			var a = 1;
+			Assert.AreEqual(a, 1);
+		}
+	}
+}
+
