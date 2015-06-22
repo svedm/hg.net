9beb3d71489e87d01c391816ba41c7e50580c47c
Svetoslav Karasev <venom008@gmail.com>
2015-06-22 23:17:00 +0500

    Hg interprocess interaction + command server handshake via pipe

---
+++ Hg.Net/Hg.Net/Channel.cs
@@ -0,0 +1,25 @@
+﻿namespace Hg.Net
+{
+	public enum Channel
+	{
+		/// <summary>
+		/// Output channel: most of the communication happens on this channel. When running commands, output Mercurial writes to stdout is written to this channel.
+		/// </summary>
+		O = 'o',
+
+		/// <summary>
+		/// Error channel: when running commands, this correlates to stderr.
+		/// </summary>
+		E = 'e',
+
+		/// <summary>
+		/// Result channel: the server uses this channel to tell the client that a command finished by writing its return value (command specific).
+		/// </summary>
+		R = 'r',
+
+		/// <summary>
+		/// Debug channel: used when the server is started with logging to '-'.
+		/// </summary>
+		D = 'd'
+	}
+}
--- Hg.Net/Hg.Net/HgClient.cs
+++ Hg.Net/Hg.Net/HgClient.cs
@@ -1,13 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
 
 namespace Hg.Net
 {
 	public class HgClient
 	{
+		public List<string> Capabilities { get; private set; }
+		public string HgEncoding { get; private set; }
+		private readonly string _hgPath = "hg";
+		private const byte HeaderLength = 5;
+		private Process _cmdServer;
+
 		public HgClient()
 		{
-			
+
 		}
+
+		public HgClient(string hgPath)
+			: this()
+		{
+			_hgPath = hgPath;
+		}
+
+		public bool Connect(string pathToRepo)
+		{
+			var args = string.Format(@"serve --cmdserver pipe --cwd ""{0}"" --repository ""{0}""", pathToRepo);
+			var serverInfo = new ProcessStartInfo(_hgPath, args)
+			{
+				RedirectStandardInput = true,
+				RedirectStandardError = true,
+				RedirectStandardOutput = true,
+				CreateNoWindow = true,
+				UseShellExecute = false
+			};
+
+			try
+			{
+				_cmdServer = Process.Start(serverInfo);
+			}
+			catch (Exception) { return false; }
+
+			return Hello();
+		}
+
+		private bool Hello()
+		{
+			var buffer = new byte[HeaderLength];
+			var readCount = ReadBytes(_cmdServer.StandardOutput.BaseStream, buffer, 0, HeaderLength);
+
+			if (readCount != HeaderLength)
+			{
+				return false;
+			}
+
+			var messageLen = GetMessageLength(buffer, 1);
+
+			var messageBuffer = new byte[messageLen];
+
+			readCount = ReadBytes(_cmdServer.StandardOutput.BaseStream, messageBuffer, 0, messageLen);
+
+			if (readCount != messageLen)
+			{
+				return false;
+			}
+
+			var message = Encoding.UTF8.GetString(messageBuffer);
+
+			var parsedMessage = message.Split('\n')
+				.Select(s => s.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries)).ToDictionary(t => t[0], t => t[1]);
+
+			Capabilities = parsedMessage["capabilities"].Split(' ').ToList();
+			HgEncoding = parsedMessage["encoding"];
+
+			return true;
+		}
+
+
+		private int ReadBytes(Stream stream, byte[] buffer, int offset, int length)
+		{
+			var remaining = length;
+			var read = 1;
+
+			while (remaining > 0 && read > 0)
+			{
+				read = stream.Read(buffer, offset, remaining);
+				offset += read;
+				remaining -= read;
+			}
+
+			return length - remaining;
+		}
+
+		private int GetMessageLength(byte[] buffer, int offset)
+		{
+			return IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buffer, offset));
+		}
+
 	}
 }
 
